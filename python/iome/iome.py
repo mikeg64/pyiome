dec4354869cc257ed605758d997eea85f7dcb76c
mikeg2105 <mikeg2105@gmail.com>
2019-02-25 15:56:05 +0000

    Command input processing written as iomelibrary processinputs function

--- python/iome/iome.py
+++ python/iome/iome.py
@@ -82,3 +82,88 @@ def loadiome(xmlfile):
 
 def writeiome(xmlfile, tree):
     tree.write(xmlfile)
+    
+def processinputs(filetemplate, fileoutput, inputparameters):
+    
+    #load dictionary of elements
+    #xmlfile="testsimfile.xml"
+    xmldic=loadiome(filetemplate)
+    
+    props=xmldic[1]
+    tree=xmldic[0]
+    
+    #tree = ET.parse("simfile.xml")
+    #root = tree.getroot()
+    
+    #print(props)
+    #user={}
+    
+    root = tree.getroot()
+    #print('here1')
+    #propse=ET.SubElement(root,'props')
+    #print('here2')
+    i=0
+    #user = {'useremail': 'm.griffiths@sheffield.ac.uk','jobtype': 'QH', 'imagefile': '126519929316b_cal_1.png'}
+    #inparams=ast.literal_eval(request.args.get('data', ''))
+    #print("inparams")
+    #print(len(inparams))
+    #print(inparams)
+
+    #only remove elements from the xml tree if the 
+    #dictionary fields are in the xml file
+    #for child in root.iter('prop'):
+    #    for val in child.iter('string'):
+    #        attribs=child.attrib
+    #        sname=attribs['name']
+            #for dname in inparams:
+            #    if dname == sname:
+            #        child.remove(val)
+        
+    
+    for prope in root.iter('prop'):
+    #for i in range(len(props)-1):
+        prop=props[i]
+        
+        i=i+1
+        try:
+            #user[prop['name']]=inparams[prop['name']]
+            
+            #prope=ET.SubElement(propse,'prop')
+            
+            #print('prope')
+            #print(prope)
+            #print("%s - %s -%s" % (prope.tag, prope.text, prope.attrib))
+            #nstringe=stringe
+            #prope.remove(stringe)
+            #propse.remove(prope)
+            
+            #print("before %s - %s - %s" % (stringe.tag, stringe.text, stringe.attrib))
+            #stringe.clear()
+            #prope.set(prop['name'],inparams[prop['name']])
+            for val in prope.iter('string'):
+                attribs=prope.attrib
+                sname=attribs['name']
+                for dname in inputparameters:
+                    print("sname and dname")
+                    print(dname)
+                    print(sname)
+                    if dname == sname:
+                        prope.remove(val)  #remove the element because we are about to add a new one
+                        stringe=ET.SubElement(prope,'string')                                                
+                        stringe.text=inputparameters[prop['name']]
+                        print("after %s - %s - %s" % (stringe.tag, stringe.text, stringe.attrib))
+                        print(stringe.text)
+                        print('set attrib')
+                        print(prop['name'])
+                        print(inputparameters[prop['name']])
+            
+        except:
+            print('except')
+            #print(user[prop['name']])
+            #user[prop['name']]='test.default'
+            
+    
+    #print(inparams)
+    writeiome(fileoutput,tree)
+    
+    return 0
--- python/pyiomes.py
+++ python/pyiomes.py
@@ -68,7 +68,7 @@ Or from python if you have the requests library installed:
 from flask import Flask, request
 import ast
 import sys
-import xml.etree.ElementTree as ET  #used by loadiome
+#import xml.etree.ElementTree as ET  #used by loadiome
 import iome.iome as io
 
 app = Flask(__name__)
@@ -99,86 +99,17 @@ def submit():
 
     #load dictionary of elements
     xmlfile="testsimfile.xml"
-    xmldic=io.loadiome(xmlfile)
+    inparams=ast.literal_eval(request.args.get('data', ''))    
     
-    props=xmldic[1]
-    tree=xmldic[0]
-    
-    #tree = ET.parse("simfile.xml")
-    #root = tree.getroot()
-    
-    #print(props)
-    #user={}
-    
-    root = tree.getroot()
-    #print('here1')
-    #propse=ET.SubElement(root,'props')
-    #print('here2')
-    i=0
-    #user = {'useremail': 'm.griffiths@sheffield.ac.uk','jobtype': 'QH', 'imagefile': '126519929316b_cal_1.png'}
-    inparams=ast.literal_eval(request.args.get('data', ''))
-    print("inparams")
-    print(len(inparams))
-    print(inparams)
-
-    #only remove elements from the xml tree if the 
-    #dictionary fields are in the xml file
-    #for child in root.iter('prop'):
-    #    for val in child.iter('string'):
-    #        attribs=child.attrib
-    #        sname=attribs['name']
-            #for dname in inparams:
-            #    if dname == sname:
-            #        child.remove(val)
+    if io.processinputs(xmlfile, 'simfile.xml', inparams) == 0:
+        print("Processed inputs")
+    else:
+        return("Processing fault: Check input string")
         
+    #Next do the job submission
     
-    for prope in root.iter('prop'):
-    #for i in range(len(props)-1):
-        prop=props[i]
-        
-        i=i+1
-        try:
-            #user[prop['name']]=inparams[prop['name']]
-            
-            #prope=ET.SubElement(propse,'prop')
-            
-            #print('prope')
-            #print(prope)
-            #print("%s - %s -%s" % (prope.tag, prope.text, prope.attrib))
-            #nstringe=stringe
-            #prope.remove(stringe)
-            #propse.remove(prope)
-            
-            #print("before %s - %s - %s" % (stringe.tag, stringe.text, stringe.attrib))
-            #stringe.clear()
-            #prope.set(prop['name'],inparams[prop['name']])
-            for val in prope.iter('string'):
-                attribs=prope.attrib
-                sname=attribs['name']
-                for dname in inparams:
-                    print("sname and dname")
-                    print(dname)
-                    print(sname)
-                    if dname == sname:
-                        prope.remove(val)  #remove the element because we are about to add a new one
-                        stringe=ET.SubElement(prope,'string')                                                
-                        stringe.text=inparams[prop['name']]
-                        print("after %s - %s - %s" % (stringe.tag, stringe.text, stringe.attrib))
-                        print(stringe.text)
-                        print('set attrib')
-                        print(prop['name'])
-                        print(inparams[prop['name']])
-            
-        except:
-            print('except')
-            #print(user[prop['name']])
-            #user[prop['name']]='test.default'
-            
-    
-    print(inparams)
-    io.writeiome('simfile.xml',tree)
-    
-    return prop['name']
+    print("Job submitted")
+    return "Jobsubmitted"
 
 
 
