744e95e47ae2f8e7a1a81a979b31151e8074314b
mikeg64 <m.griffiths@sheffield.ac.uk>
2019-03-14 16:42:39 +0000

    invoking a server less function from python script

--- initialtests/temp.py
+++ initialtests/temp.py
@@ -5,6 +5,33 @@ Spyder Editor
 This is a temporary script file.
 """
 import requests
+
+""" another example """
+"""import requests"""
+
+""" a post request example"""
+
+"""
+curl -d @request.json --header "Content-Type: application/json" https://www.googleapis.com/qpxExpress/v1/trips/search?key=mykeyhere
+"""
+url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=mykeyhere'
+payload = open("request.json")
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+r = requests.post(url, data=payload, headers=headers)
+
+
+"""
+could invoke fn like server payload as
+
+curl -H "Content-Type: application/json" -d '{"name":"Bob"}' http://localhost:8080/t/pythonapp/pythonfn-trigger
+"""
+"""
+see link
+https://fnproject.io/tutorials/python/intro/
+"""
+
+
+
 r = requests.get("http://localhost:5000/submit?data={'useremail': 'mikeg2107@gmail.com', 'jobtype': 'SH', 'imagefile': 'testimage2.jpg'}")
 #r = requests.get("http://localhost:5000/submit?data={ 'jobtype': 'QH', 'imagefile': 'testimage3.jpg'}")
 
@@ -15,3 +42,7 @@ r = requests.get("http://localhost:5000/submit?data={'useremail': 'mikeg2107@gma
 
 
 print(r)
+
+
+
+
