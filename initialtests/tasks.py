3f106724ef87ee3b6f4bf3b19a7a5a46d92918dc
Ubuntu <ubuntu@ip-172-31-4-236.eu-west-1.compute.internal>
2019-06-03 19:27:54 +0000

    corrections to enable use of redis

--- initialtests/tasks.py
+++ initialtests/tasks.py
@@ -7,7 +7,11 @@ Created on Fri Feb 22 03:01:04 2019
 #see comments on celery1.py
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+#rabbit mq server
+#app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+#redis server
+app = Celery('tasks', broker='redis://localhost//')
 
 @app.task
 def add(x, y):
